2c848376c32a8a48dcee31993647f8e55fd7420d
akkaungkyawkhaing <aungkyawkhaing22july@gmail.com>
2019-11-26 00:38:58 +0630

    Updates

---
+++ precache-manifest.868da49d8478a78689545662c2fa1135.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4452fb8a070133c4e565",
+    "url": "/akkdesign/static/js/runtime~main.4452fb8a.js"
+  },
+  {
+    "revision": "38e64b2f781289360be5",
+    "url": "/akkdesign/static/js/main.38e64b2f.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/akkdesign/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "38e64b2f781289360be5",
+    "url": "/akkdesign/static/css/main.76ec4c38.chunk.css"
+  },
+  {
+    "revision": "7f067a99daa12677224e62b9d1a9d6db",
+    "url": "/akkdesign/index.html"
+  }
+];
