d07e2666cf02e3181e447739c2104fbfd37e63b4
akkaungkyawkhaing <aungkyawkhaing22july@gmail.com>
2019-11-28 02:44:04 +0630

    change

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -39,6 +39,7 @@ export default class Contact extends Component {
                                     </div>
                                 </div>
                             </div>
+
                             <div className="col-md-7 col-md-push-1">
                                 <div className="row">
                                     <div className="col-md-10 col-md-offset-1 col-md-pull-1 animate-box" data-animate-effect="fadeInRight">
@@ -63,9 +64,36 @@ export default class Contact extends Component {
                                 </div>
                             </div>
                         </div>
+
+                        {/* <div className="row">
+                            <div className="col-md-5">
+                                <div className="row">
+                                    <div className="col-md-10 col-md-offset-1 col-md-pull-1 animate-box" data-animate-effect="fadeInRight">
+                                        <form action="">
+                                            <div className="form-group">
+                                                <input type="text" className="form-control" placeholder="Name" />
+                                            </div>
+                                            <div className="form-group">
+                                                <input type="text" className="form-control" placeholder="Email" />
+                                            </div>
+                                            <div className="form-group">
+                                                <input type="text" className="form-control" placeholder="Subject" />
+                                            </div>
+                                            <div className="form-group">
+                                                <textarea name="" id="message" cols="30" rows="7" className="form-control" placeholder="Message"></textarea>
+                                            </div>
+                                            <div className="form-group">
+                                                <input type="submit" className="btn btn-primary btn-send-message" value="Send Message" />
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> */}
+
                     </div>
                 </section>
-            </div>
+            </div >
         )
     }
 }
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,14 +8,15 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
+              <div className="author-img" style={{ backgroundImage: 'url(images/about.jpg)' }} />
               <h1 id="colorlib-logo"><a href="index.html">Aung Kyaw Khaing</a></h1>
               <span className="position">
                 <a href="#position">Dev </a>
-                 in myanmar
+                in myanmar
               </span>
               {/* <span className="email"><i className="icon-mail"></i> @gmail.com</span> */}
             </div>
+            <div>
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
               <div id="navbar" className="collapse">
                 <ul>
@@ -29,6 +30,7 @@ export default class Sidebar extends Component {
                 </ul>
               </div>
             </nav>
+            </div>
             {/* <nav id="colorlib-main-menu">
               <ul>
                 <li><a href="#position" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
@@ -43,19 +45,16 @@ export default class Sidebar extends Component {
               <p>
                 <small>
                   ©
-                  Copyright 
-                  © 
+                  Copyright
+                  ©
                   <script type="text/javascript">document.write(new Date().getFullYear());</script>
                   2019
                   All rights reserved |
                   Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-coffee" aria-hidden="true"></i>
                   <br></br>
-                  Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration 
+                  Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration
               </small>
               </p>
-              {/* <p><small>
-                Something coming soon !!
-              </small></p> */}
             </div>
           </aside>
         </div>
