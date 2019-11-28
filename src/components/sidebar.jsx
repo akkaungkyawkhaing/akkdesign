453d230dfe605cd7449c34a87f6cae123df47232
akkaungkyawkhaing <aungkyawkhaing22july@gmail.com>
2019-11-28 21:00:20 +0630

    change

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -48,7 +48,7 @@ export default class Sidebar extends Component {
                  All rights reserved // <br></br>
                   Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-coffee" aria-hidden="true"></i>
                   <br></br>
-                  Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration
+                  Thank <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration
               </small>
               </p>
             </div>
