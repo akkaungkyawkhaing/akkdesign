50bb4dec742e8fec4ea8f5f59c7a0b0d7310ac8c
akkaungkyawkhaing <aungkyawkhaing22july@gmail.com>
2019-12-01 02:25:14 +0630

    notfound

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import NotFound from './components/NotFound';
+import { Router, Route, browserHistory } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () =>
+  
+<Router history={browserHistory}>
+<Route path="/" component={App}>
+
+  {/* Your other routes here */}
+
+  <Route path="" component={NotFound} />
+</Route>
+</Router>
+ 
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // index.html inside to App or Class
 
