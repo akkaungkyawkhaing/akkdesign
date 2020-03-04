80e2fdf40ca5167fe37202340ddfc9fa6b770e43
akkaungkyawkhaing <aungkyawkhaing22july@gmail.com>
2020-03-04 22:53:04 +0630

    Updates

---
+++ precache-manifest.cb21bb44efb902d59a7268dd785a4fbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ac5e002a9e056995b4d8",
+    "url": "/static/js/main.ac5e002a.chunk.js"
+  },
+  {
+    "revision": "381c072bbb957b78ba0c",
+    "url": "/static/js/2.381c072b.chunk.js"
+  },
+  {
+    "revision": "ac5e002a9e056995b4d8",
+    "url": "/static/css/main.76ec4c38.chunk.css"
+  },
+  {
+    "revision": "87cbaf6372af72bcd9583cdc1e7541ed",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40b0c00eb0cd8c3fd6740198ee03b92a.js"
+  "/precache-manifest.cb21bb44efb902d59a7268dd785a4fbd.js"
 );
 
 workbox.clientsClaim();
